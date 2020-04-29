b1a611bfbf478066e35edcde8330971815086ffd
Pflaumenpauli <32703585+Pflaumenpauli@users.noreply.github.com>
2020-04-29 08:20:04 +0200

    Update CalculatorHandler.kt
    add offline function in CalculatorHandler

--- app/src/main/java/com/kp/loomo/features/intents/handler/CalculatorHandler.kt
+++ app/src/main/java/com/kp/loomo/features/intents/handler/CalculatorHandler.kt
@@ -6,6 +6,21 @@ import com.kp.loomo.features.intents.IntentMessageHandler
 private const val TAG = "CalculatorHandler"
 
 class CalculatorHandler : IntentMessageHandler {
+   
+    private val keywords = arrayOf(
+	"plus", "sum", "add", "addition",
+	"minus", "difference", "substraction", "subtract",
+	"multiplication", "times", "product",
+	"quotient", "division", "divided by"
+    )
+
+    private val firstNumbers = arrayOf(
+	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+    )
+
+    private val secondNumbers = arrayOf(
+	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+    )
 
     override fun canHandle(intentMessage: DetectIntentResponse): Boolean {
         return intentMessage.queryResult.intent.displayName == "calculate"
@@ -34,11 +49,107 @@ class CalculatorHandler : IntentMessageHandler {
 		}   
     }
 
-    override fun canHandleOffline(intentMessage: String): Boolean {
-        return false
+	
+
+    override fun canHandleOffline (intentMessage: String): Boolean{
+	for (keywords in keyword){
+		if (intentMessage.contains(keyword, true) {
+			return true 
+		}
+	}
+	return false
     }
 
-    override fun handleOffline(intentMessage: String): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun handleOffline (intentMessage: String): String {
+	for (keyword in keywords) {
+		if (keyword == "plus" || keyword == "sum" || keyword == "add" || keyword == "addition") {
+			if (intentMessage.contains(keyword, true) {
+				for (firstNumber in firstNumbers)
+					if (intentMessage.contains(firstNumber, true) {
+						val firstValue = mapStringToNumber(firstNumber)
+						for (secondNumber in secondNumbers) {
+							if (intentMessage.contains(secondNumber, true) {
+								val secondValue = mapStringToNumber(secondNumber)
+								val result = firstValue + secondValue
+								return "It's $result, right?"
+							}
+						}
+						return "Sorry, I didn't get the second number."
+					}
+				}
+				return "'Sorry, I didn't get that."
+			}
+		}
+		if (keyword == "minus" || keyword == "difference" || keyword == "substraction" || keyword == "subtract") {
+			if (intentMessage.contains(keyword, true) {
+				for (firstNumber in firstNumbers) {
+					if (intentMessage.contains(firstNumber, true)
+						val firstValue = mapStringToNumber(firstNumber)
+						for (secondNumber in secondNumbers) {
+							if (intentMessage.contains(secondNumber, true) {
+								val secondValue = mapStringToNumber(secondNumber)
+								val result = firstValue - secondValue
+								return "It's $result, right?"
+							}
+						}
+						return "Sorry, I didn't get the second number."
+					}
+				}
+				return "Sorry, I din't get that."
+			}		
+		}
+		if (keyword == "multiplication" || keyword == "times" || keyword == " product") {
+			if (intentMessage.contains(keyword, true) {
+				for (firstNumber in firstNumbers) {
+					if (intentMessage.contains(firstNumber, true) {
+						val firstValue = mapStringToNumber(firstNumber)
+						for(secondNumber in secondNumbers) {
+							if (intentMessage.contains(secondNumber, true) {
+								val secondValue = mapStringToNumber(secondNumber)
+								val result = firstValue * secondValue
+								return "It's $result, right?"
+							}
+						}
+						return "Sorry, I didn't get the second number."
+					}
+				}
+				return "Sorry, I didn't get that."
+			}
+		}
+		if (keyword == "quotient" || keyword == "division" || keyword == "divided by") {
+			if (intentMessage.contains(keyword, true) {
+				for (firstNumber in firstNumbers) {
+					if (intentMessage.contains(firstNumber, true) {
+						val firstValue = mapStringToNumber(firstNumber)
+						for (secondNumber in secondNumbers) {
+							if (intentMessage.contains(secondNumber, true) {
+								val secondValue = mapStringToNumber(secondNumber)
+								val result = firstValue / secondValue
+								return "It's $result, right?"
+							}
+						}
+						return "Sorry, I didn't get the second number."
+					}
+				}
+				return "Sorry, I didn't get that."
+			}
+		}
+	}
     }
+
+    private fun mapStringToNumber (stringNumber: String): Int {
+	when (stringNumber) {
+		"zero" -> return 0
+		"one" -> return 1
+		"two" -> return 2
+		"three" -> return 3
+		"four" -> return 4
+		"five" -> return 5
+		"six" -> return 6
+		"seven" -> return 7
+		"eight" -> return 8
+		"nine" -> return 9
+		"ten" -> return 10
+	}
+    }	
 }
